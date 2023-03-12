008b8c014a5f62b8b0fb7e108eb1841b5da57ff5
Polina <polinazholudeva511256@gmail.com>
2023-03-12 20:26:12 +0300

    feat: task is ready for finalwork

---
+++ finaltask1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива
+// строк формирует массив из строк, длина которых меньше или равна 3 символа.
+
+string [] inputArray = new string[4] {"rggtg", "tgukuoil", "pol", "dg"};
+string [] outputArray = new string[inputArray.Length];
+int indexOut=0;
+for (int indexIn = 0; indexIn < inputArray.Length; indexIn++)
+{
+    if (inputArray[indexIn].Length<=3)
+    {
+        outputArray[indexOut]=inputArray[indexIn];
+        indexOut=indexOut+1;
+    }
+}
+
+
+void PrintArray(string[] array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i<size-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
+PrintArray (outputArray, indexOut);
